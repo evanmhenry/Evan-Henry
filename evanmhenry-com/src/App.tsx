ccba6357e876836c739478e8a06784f618c7c9e8
Evan Henry <evan@spadehq.com>
2023-02-05 21:05:25 -0500

    fix tailwind linking issue

--- evanmhenry-com/src/App.tsx
+++ evanmhenry-com/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 function App() {
-	return <div className=''>start here</div>
+	return <div className='bg-red-500'>start here</div>
 }
 
 export default App
--- evanmhenry-com/tailwind.config.js
+++ evanmhenry-com/tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
+	mode: 'jit',
+	theme: {
+		screens: {
+			xs: '480px',
+			ss: '620px',
+			sm: '768px',
+			md: '1060px',
+			lg: '1200px',
+			xl: '1700px',
+		},
+	},
+	plugins: [],
 }

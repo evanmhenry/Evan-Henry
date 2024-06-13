9fe03f62b5b3830f46666834815296b69044abfe
Evan Henry <evan@spadehq.com>
2024-06-13 17:36:38 -0400

    fix build error

--- evanmhenry-com/src/components/navigation/web-layout/Footer.tsx
+++ evanmhenry-com/src/components/navigation/web-layout/Footer.tsx
@@ -1,6 +1,4 @@
-import { useTheme } from '@/components/theming/ThemeProvider'
 import { useNavigate } from 'react-router-dom'
-import LightLogo from '@/assets/brand/spade-software-logo-light.svg'
 import DarkLogo from '@/assets/brand/spade-software-logo-dark.svg'
 import InstagramDark from '@/assets/social-icons/instagram-dark.svg'
 import TwitterDark from '@/assets/social-icons/twitter-dark.svg'
@@ -8,14 +6,13 @@ import YouTubeDark from '@/assets/social-icons/youtube-dark.svg'
 import GitHubDark from '@/assets/social-icons/github-dark.svg'
 
 const Footer = () => {
-	const { theme } = useTheme()
 	const navigate = useNavigate()
 
 	return (
 		<footer className='w-full border-t border-light/10'>
 			<div className='container max-w-screen-2xl flex flex-col md:flex-row items-start py-12 gap-y-8 gap-x-4 md:gap-x-8 lg:gap-x-20 '>
 				<div className='flex flex-col items-center w-full gap-y-8 flex-1 md:w-40 lg:w-60'>
-					<img alt='Logo' className='h-8 flex-none hover:cursor-pointer' src={theme === 'dark' ? DarkLogo : LightLogo} onClick={() => navigate('/')} />
+					<img alt='Logo' className='h-8 flex-none hover:cursor-pointer' src={DarkLogo} onClick={() => navigate('/')} />
 					<div className='flex gap-4'>
 						<a href='https://www.instagram.com/' target='_blank' rel='noopener noreferrer'>
 							<img src={InstagramDark} alt='Instagram' className='h-5 w-5' />
